40260437412eddb25968ef61029569574c759a2b
Tomasz Maciążek <tommac.opole@gmail.com>
2024-02-24 13:53:41 +0100

    Initial solution structure

---
+++ src/GatewayManager.Logic/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GatewayManager.Logic
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddLogic(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
